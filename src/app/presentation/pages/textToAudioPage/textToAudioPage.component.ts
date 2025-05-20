a5f1d32e77b541d7ae0e721ecaf87d00543e05ad
santyxxo <betancourtlopezjorge@gmail.com>
2025-05-20 13:12:19 -0500

    modulo de texto a audio

--- src/app/presentation/pages/textToAudioPage/textToAudioPage.component.ts
+++ src/app/presentation/pages/textToAudioPage/textToAudioPage.component.ts
@@ -1,13 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-@Component( {
+@Component({
   selector: 'app-text-to-audio-page',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule, 
   ],
   templateUrl: './textToAudioPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-} )
-export default class TextToAudioPageComponent { }
+})
+export default class TextToAudioPageComponent {
+  userText = '';
+
+  speakText() {
+    if (!this.userText.trim()) {
+      alert('Por favor, escribe algún texto.');
+      return;
+    }
+
+    if ('speechSynthesis' in window) {
+      const utterance = new SpeechSynthesisUtterance(this.userText);
+      utterance.lang = 'es-ES'; 
+      window.speechSynthesis.speak(utterance);
+    } else {
+      alert('Tu navegador no soporta texto a voz.');
+    }
+  }
+}
