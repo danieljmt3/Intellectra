08aa74a23abd29a77090b0717ffd5174cba729fc
Edwin <edwinelcrak40@gmail.com>
2025-08-01 16:38:10 -0500

    Arreglo de modulos frontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,28 +17,6 @@ export const routes: Routes = [
         },
       },
       {
-        path: 'pros-cons',
-        loadComponent: () =>
-          import('./presentation/pages/prosConsPage/prosConsPage.component'),
-        data: {
-          icon: 'fa-solid fa-code-compare',
-          title: 'Pros & Cons',
-          description: 'Comparar pros y contras',
-        },
-      },
-      {
-        path: 'pros-cons-stream',
-        loadComponent: () =>
-          import(
-            './presentation/pages/prosConsStreamPage/prosConsStreamPage.component'
-          ),
-        data: {
-          icon: 'fa-solid fa-water',
-          title: 'Como stream',
-          description: 'Con stream de mensajes',
-        },
-      },
-      {
         path: 'translate',
         loadComponent: () =>
           import('./presentation/pages/translatePage/translatePage.component'
@@ -63,18 +41,6 @@ export const routes: Routes = [
         },
       },
       {
-        path: 'audio-to-text',
-        loadComponent: () =>
-          import(
-            './presentation/pages/audioToTextPage/audioToTextPage.component'
-          ),
-        data: {
-          icon: 'fa-solid fa-comment-dots',
-          title: 'Audio a texto',
-          description: 'Convertir audio a texto',
-        },
-      },
-      {
         path: 'image-generation',
         loadComponent: () =>
           import(
@@ -86,19 +52,6 @@ export const routes: Routes = [
           description: 'Generar imágenes',
         },
       },
-      {
-        path: 'image-tunning',
-        loadComponent: () =>
-          import(
-            './presentation/pages/imageTunningPage/imageTunningPage.component'
-          ),
-        data: {
-          icon: 'fa-solid fa-wand-magic',
-          title: 'Editar imagen',
-          description: 'Generación continua',
-        },
-      },
-
       {
         path: 'assistant',
         loadComponent: () =>
