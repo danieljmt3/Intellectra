29d8c3dd5fb5de11168e250ed6ef0b1731687599
Edwin <edwinelcrak40@gmail.com>
2025-09-03 12:22:44 -0500

    Arreglos finales para producción

--- src/app/presentation/services/openai.service.ts
+++ src/app/presentation/services/openai.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class OpenAiService {
-  private apiURL = 'http://localhost:3000/intellectra'; // URL del backend
+  private apiURL = environment.apiurl; // URL del backend
 
   constructor(private http: HttpClient) {}
 
